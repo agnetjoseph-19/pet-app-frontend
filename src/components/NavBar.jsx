99a4000ce9c359b61004e53d4a5845d836a897c2
agnetjoseph@gmail.com <agnetjoseph@gmail.com>
2026-07-16 00:26:22 +0530

    nav completed

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Pet Boarding Management</a>
+
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <Link className="nav-link active" aria-current="page" to="/">Add Pet</Link>
+                            <Link className="nav-link active" aria-current="page" to="/view">View Pets</Link>
+
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
